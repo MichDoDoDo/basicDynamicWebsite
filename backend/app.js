3a275f528209c784b0a9381eed0cbb803fb093a2
michael <michaeldo495@gmail.com>
2025-05-12 17:37:05 -0700

    add api endpoints

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { connectDataBase } from "./DB/db.js";
 import Products from "./DBModels/Product_Model.js";
+import mongoose from "mongoose";
 const app = express();
 
 app.use(express.json());
@@ -19,7 +20,68 @@ app.post("/api/products", async(req,res) => {
         res.status(500).json({message: "Error creating product", error});
     }
 });
-app.listen(5000, () =>{
+
+app.get("/api/products", async(req,res) => {
+    try{
+        const product = await Products.find({});
+        res.status(200).json({success:true, data:product})
+        
+    }catch(error){
+        
+        res.status(500).json({message: "Error getting products", error});
+    }
+});
+app.get("/api/products/:id", async(req,res) => {
+    const {id} = req.params;
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({message:`not valid id ${id}`});
+    }
+    try{
+        const product = await Products.findById(id);
+        if(!product)
+        {
+            return res.status(404).json({message:`Could not find item with ${id}`})
+        }
+        res.status(200).json({success:true, message:`Found item with ${id}`, data:product})
+    }catch(error){
+        res.status(500).json({message:"Server Error"})
+
+    }
+});
+
+app.delete("/api/products/:id", async(req,res) => {
+    const {id} = req.params;
+    try{
+        const product = await Products.findByIdAndDelete(id);
+        if(!product){
+            return res.status(404).json({message: "Product not found"});
+        }
+        res.status(200).json({message: "Product deleted successfully", product});
+    }
+    catch(error){
+        res.status(500).json({message: "Error deleting product", error});
+    }   
+});
+
+app.put("/api/products/:id", async(req,res) => {
+
+    const {id} = req.params;
+    const productInfo = req.body;
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({message:`not valid id ${id}`});
+    }
+    try{
+        const updatedProduct = await Products.findByIdAndUpdate(id, productInfo,{new:true});
+        res.status(200).json({success: true, data: updatedProduct})
+    }
+    catch(error)
+    {
+        res.status(500).json({success: false, message:"Internal server error"})
+    }
+
+});
+
+app.listen(8000, () =>{
     console.log("localhost:5000 has been started hello");
     if(connectDataBase()){
         console.log("Database connection established");
